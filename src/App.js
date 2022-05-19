7f09324916e5bc0b423fa4ed5b0c213439a5a130
chocoboabby <93240698+chocoboabby@users.noreply.github.com>
2022-05-19 11:16:21 +0800

    add bootsrap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello Test</h1>
-      <div>
-        <a
-          href="https://github.com/chocoboabby/abbyreact-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code
-        </a>{" "}
-        by Abby Dela Fuente
+    <div className="App">
+      <div className="container">
+        <h1>Hello Test</h1>
+        <div>
+          <a
+            href="https://github.com/chocoboabby/abbyreact-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by Abby Dela Fuente
+        </div>
       </div>
     </div>
   );
